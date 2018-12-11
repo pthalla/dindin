03fb7bcf83bb3fd14cb4ad18a0c3e938f5f5f935
Praneeth Thalla <pthalla1@gmail.com>
2018-12-11 01:47:55 -0500

    Updated HTML fields with Yelp API data

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const request = require('request')
+const auth = require('./config')
+
+const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('public'))
+
+app.get("/randomfood", (req, res, next) => {
+  latitude = req.query.latitude
+  longitude = req.query.longitude
+  url = "https://api.yelp.com/v3/businesses/search?" + "term=food" + "&latitude=" + latitude + "&longitude=" + longitude
+  
+  var options = {
+    url: url,
+    headers: {
+      "Authorization": auth
+    }
+  }
+
+  request(options, (err, response, body) => {
+    if (err) return console.log(err)
+    var businesses = JSON.parse(body)
+    var restaurantArr = businesses["businesses"];
+    var randNum = Math.floor((Math.random() * restaurantArr.length) + 1)
+    var restaurant = restaurantArr[randNum]
+    din = {
+      image: restaurant["image_url"],
+      name: restaurant["name"],
+      phone: restaurant["phone"],
+      website: restaurant["url"]
+    }
+
+    res.send(din)
+  })
+  
+})
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Dindin listening on port ${port}`)
+})
+
---
+++ public/displayfood.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      url = "http://localhost:3000/randomfood" + "?latitude=" + position.coords.latitude + "&longitude=" + position.coords.longitude
+      $("#landingholder").show()
+      $.get(url, function(res) {
+        $("#answer").text(res.name + " is for din din")
+        $("#dinimage").attr("src", res.image)
+        $("#dinname").text(res.name)
+        $("#dinphone").text(res.phone)
+        $("#dinwebsite").attr("href", res.website)
+        console.log(res)
+      });
+    }, function(err) {
+      console.log(err) // TODO: better error handling
+    });
+  } else {
+    $("#answer").text("Geolocation is not supported by this browser");
+  } 
+
+  
+});
+
+/*
+Image: "image_url": "http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg",
+
+Name: "name": "Four Barrel Coffee"
+
+Phone: "phone": "+14152520800"
+
+Website: "url": "https://www.yelp.com/biz/four-barrel-coffee-san-francisco"
+
+
+*/
+
+
+
